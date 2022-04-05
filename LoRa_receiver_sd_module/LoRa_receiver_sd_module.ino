85302868f10338ba3ad40ae4dad6c860ed1924fb
A.W.J. van Kasteren <bvankast@gmail.com>
2022-04-05 10:19:18 +0200

    Initial receiver and sender codes

---
+++ LoRa_receiver_sd_module/LoRa_receiver_sd_module.ino
@@ -0,0 +1,100 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+#include <SD.h>
+File myFile;
+
+//define the pins used by the transceiver module
+#define ss 10
+#define rst 9
+#define dio0 2
+#define CHIP_SELECT_SD 3
+
+String file_name_A = "Receiver_a";
+String file_name_B = ".txt";
+
+String file_name = file_name_A + file_name_B;
+int session_identifier = random(11111,99999);
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(9600);
+  Serial.println(file_name);
+  Serial.print("Receiver session ID:"); Serial.println(session_identifier);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(433E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+  
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(CHIP_SELECT_SD)) {
+  Serial.println("initialization failed!");
+  while (1);
+  }
+  Serial.println("initialization done.");
+  myFile = SD.open("ReceiverA.txt", FILE_WRITE); // check whats happening here
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to SD...");
+    myFile.println("Receiver session ID:");
+    myFile.println(session_identifier);
+    myFile.close();
+    Serial.println("done.");
+  } else {
+  // if the file didn't open, print an error:
+  Serial.println("error opening SD file");
+  }
+}  
+
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(LoRaData);
+
+      
+      myFile = SD.open(file_name, FILE_WRITE);
+      if (myFile) {
+      myFile.print(LoRaData);
+      myFile.close();
+      }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    myFile = SD.open(file_name, FILE_WRITE);
+     if (myFile) {
+     myFile.print("' with RSSI: ");
+     myFile.print(LoRa.packetRssi());
+     myFile.close();
+     }
+  }
+  }
+}
---
+++ LoRa_sender/LoRa_sender.ino
@@ -0,0 +1,61 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the transceiver module
+#define ss 10
+#define rst 9
+#define dio0 2
+
+int counter = 0;
+long session_identifier =0;
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(433E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+
+  session_identifier = random(11111,99999);
+  Serial.print("Session identifier: "); Serial.println(session_identifier);
+}
+
+void loop() {
+  //Serial.print("Session identifier: "); Serial.println(session_identifier);
+  
+  Serial.print("Sending packet: "); Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("ID:");
+  LoRa.print(session_identifier);
+  LoRa.print(" packet_number:");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(3000);
+}
