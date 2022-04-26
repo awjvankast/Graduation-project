34c43ca6699e794c3e66a244c6027cea0f1d8fe9
A.W.J. van Kasteren <bvankast@gmail.com>
2022-04-26 18:59:57 +0200

    first working version IMU sender

---
+++ LoRa_receiver_display/LoRa_receiver_display.ino
@@ -0,0 +1,108 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+#include <SD.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+//define the pins used by the transceiver module
+#define ss 10
+#define rst 9
+#define dio0 2
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(9600);
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+    display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Initializing display...");
+  display.display(); 
+  
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+  display.println("LoRa Receiver");
+  display.display(); 
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(433E6)) {
+    Serial.println(".");
+    display.println(".");
+    display.display(); 
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+  display.println("LoRa Initializing OK!");
+  display.display(); 
+  delay(500);
+  display.clearDisplay();
+  display.setCursor(0, 10);
+}
+
+int line_count = 0;
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    if(line_count == 6){
+      line_count = 0;
+      display.clearDisplay();
+      display.setCursor(0, 10);
+    }
+    // received a packet
+    Serial.print("Rp:");
+    display.print("Rp:");
+    display.display(); 
+
+    // read packet
+    while (LoRa.available()) {
+      String LoRaData = LoRa.readString();
+      Serial.print(LoRaData); 
+      display.print(LoRaData);
+      display.display(); 
+    }
+
+    // print RSSI of packet
+    Serial.print(",RS:");
+    display.print(",RS:");
+    display.display(); 
+    Serial.println(LoRa.packetRssi());
+    display.println(LoRa.packetRssi());
+    display.display(); 
+
+    line_count++;
+  }
+  
+}
---
+++ LoRa_sender_IMU/LoRa_sender_IMU.ino
@@ -0,0 +1,125 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://randomnerdtutorials.com
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+#include <MPU9250_asukiaaa.h>
+#ifdef _ESP32_HAL_I2C_H_
+#define SDA_PIN 21
+#define SCL_PIN 22
+#endif
+
+MPU9250_asukiaaa mySensor;
+float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
+
+//define the pins used by the transceiver module
+#define ss 10
+#define rst 9
+#define dio0 2
+
+int counter = 0;
+long session_identifier =0;
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  #ifdef _ESP32_HAL_I2C_H_ // For ESP32
+  Wire.begin(SDA_PIN, SCL_PIN);
+  mySensor.setWire(&Wire);
+  #endif
+
+  mySensor.beginAccel();
+  mySensor.beginGyro();
+  mySensor.beginMag();
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(433E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+
+  session_identifier = random(11111,99999);
+  Serial.print("Session identifier: "); Serial.println(session_identifier);
+}
+
+void loop() {
+    uint8_t sensorId;
+  if (mySensor.readId(&sensorId) == 0) {
+    Serial.println("sensorId: " + String(sensorId));
+  } else {
+    Serial.println("Cannot read sensorId");
+  }
+
+  if (mySensor.accelUpdate() == 0) {
+    aX = mySensor.accelX();
+    aY = mySensor.accelY();
+    aZ = mySensor.accelZ();
+    aSqrt = mySensor.accelSqrt();
+    Serial.println("accelX: " + String(aX));
+    Serial.println("accelY: " + String(aY));
+    Serial.println("accelZ: " + String(aZ));
+    Serial.println("accelSqrt: " + String(aSqrt));
+  } else {
+    Serial.println("Cannod read accel values");
+  }
+
+  if (mySensor.gyroUpdate() == 0) {
+    gX = mySensor.gyroX();
+    gY = mySensor.gyroY();
+    gZ = mySensor.gyroZ();
+    Serial.println("gyroX: " + String(gX));
+    Serial.println("gyroY: " + String(gY));
+    Serial.println("gyroZ: " + String(gZ));
+  } else {
+    Serial.println("Cannot read gyro values");
+  }
+
+  if (mySensor.magUpdate() == 0) {
+    mX = mySensor.magX();
+    mY = mySensor.magY();
+    mZ = mySensor.magZ();
+    mDirection = mySensor.magHorizDirection();
+    Serial.println("magX: " + String(mX));
+    Serial.println("maxY: " + String(mY));
+    Serial.println("magZ: " + String(mZ));
+    Serial.println("horizontal direction: " + String(mDirection));
+  } else {
+    Serial.println("Cannot read mag values");
+  }
+
+  Serial.println("at " + String(millis()) + "ms");
+  Serial.println(""); // Add an empty line
+
+  
+  Serial.print("Sending packet: "); Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("ID: ");
+  LoRa.print(session_identifier);
+  LoRa.print(" packet_number: ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(3000);
+}
