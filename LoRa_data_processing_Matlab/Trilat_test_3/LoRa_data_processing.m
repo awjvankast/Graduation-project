7b84d141e9f1ebd836ef08c2df2d40d870d7ff1d
A.W.J. van Kasteren <bvankast@gmail.com>
2022-09-10 08:57:18 +0200

    Up to date from desktop

--- LoRa_data_processing_Matlab/Trilat_test_3/LoRa_data_processing.m
+++ LoRa_data_processing_Matlab/Trilat_test_3/LoRa_data_processing.m
@@ -1,46 +1,52 @@
 %close all; clear all; clc;
 
-% fill in the right path
-%data = LoRa_data_import_func_test3("..\..\data\LoRa_trilateration_test_3\Rx_green.TXT",[1 inf]);
-
+% Replace this with the data imported
 data = Rxgreen;
 
-% add a check to see if it are all numbers in the correct columns,
-% otherwise throw the garbage data away
-
-% TODO make it more robust
+% Check the data for the correct format, if a single data entry is wrong
+% throw away the entire row
 nan_check = isnan(data);
 for k = 1:size(nan_check,1)
-    if any( nan_check(k,[1,3:13]) == 1)
-        if  nan_check(k,2)
-            data(k,:) = [];
+    if any( nan_check(k,[1,3:13]) == 1) % if any of the values in these columns is NaN
+        
+        % checks if the row is a session identifier row, if not, delete the
+        % row
+        if  ~nan_check(k,2) && ~any( nan_check( k, [1,3:13] ) == 0)    
+        else
+        data(k,:) = [];
         end
+
     end
 end
     
-
-
 session_index_bin = ~isnan(data(:,2));
 session_start_index = find(session_index_bin==1);
 
 no_indices = sum(session_index_bin);
-session_data = NaN(no_indices,size(data,1),3);
+session_data = NaN(no_indices,size(data,1),size(data,2)-1);
 
 for k=1:no_indices
-    if k ~=no_indices
+    if k ~=no_indices % if it's not the last session
+        % now skip the first row of the session because only the Rx 
+        % session identifier is in there
+        % substract one fom the next start index to get the last row of the
+        % session
         rel_rows = session_start_index(k)+1:session_start_index(k+1)-1;
     else
+        % if it's the last session of the batch just go to the last element
+        % of the dataset
         rel_rows = session_start_index(k)+1:length(data);
     end
+    % Now put the data in bins per session TODO
     session_data(k,1,1) = data(session_start_index(k),2);
-    session_data(k,rel_rows,:) = data(rel_rows,[1, 3, 4]);
+    session_data(k,rel_rows,:) = data(rel_rows,[1, 3:13]);
 end
 
-relevant_no_session = 2; % fill the how many-th receiver session is relevant
+relevant_no_session = 8; % fill the how many-th receiver session is relevant
 
 data_to_use = squeeze(session_data(relevant_no_session,:,:));
 relevant_data_row_bin = ~isnan(data_to_use( :,1 ));
 filtered_data = data_to_use(relevant_data_row_bin,:)
 
 % stores data in relevant folder
-save("..\data\LoRa_trilateration_test_1\Rx1.mat",'filtered_data');
+save("..\..\data\LoRa_trilateration_test_3\Rx_ygreen_distance.mat",'filtered_data');
---
+++ LoRa_data_processing_Matlab/Trilat_test_3/Rx_data_bin_test3.m
@@ -0,0 +1,52 @@
+function [Blue_RSSI_mean] = Rx_data_bin_test3(Rx)
+% Function which takes as input a matrix with first column of ID's and the
+% next columns of packets numbers, IMU data and RSSI data
+% It outputs a matrix with the mean of half the numbers of samples per ID
+% This mean is taken over the middle 50% of the samples, the first and
+% last quarter of the samples are discarded
+
+[no_ID_not_ordered, ID_not_ordered] = groupcounts(Rx(:,1));
+Blue_ID_received = unique(Rx(:,1),'stable');
+no_ID_ordered = zeros(1,length(no_ID_not_ordered));
+
+% Neccesary to order it again because the function groupcounts() does not
+% preserve the order while unique() does
+for k = 1:length(ID_not_ordered)
+    for j = 1:length(ID_not_ordered)
+        if Blue_ID_received(k) == ID_not_ordered(j)
+            no_ID_ordered(k) = no_ID_not_ordered(j);
+        end
+    end
+end
+
+% Initializing size of array based on largest number of samples
+largest_no_ID = max(no_ID_ordered);
+Blue_RSSI = zeros(length(no_ID_ordered), largest_no_ID);
+
+% Formatting the matrix
+for k = 1:length(Blue_ID_received)
+    index_ID = Blue_ID_received(k)==Rx;
+    RSSI = Rx(index_ID(:,1),12);
+    Blue_RSSI(k,1:length(RSSI)+1) = [Blue_ID_received(k) RSSI'];
+end
+
+% Taking half of the total measurements of each ID, discarding the first
+% and last quarter of measurements
+Blue_RSSI_filtered = zeros(length(no_ID_ordered), largest_no_ID);
+Blue_RSSI_mean = zeros(length(no_ID_ordered),2);
+Blue_RSSI_filtered(:,1) = Blue_RSSI(:,1);
+Blue_RSSI_mean(:,1) = Blue_RSSI(:,1);
+for k = 1:length(no_ID_ordered)
+    tot_samples = sum(Blue_RSSI(k,:)~=0)-1;
+    samples_to_take = tot_samples/2;
+    samples_to_discard = [round(tot_samples/4) floor(tot_samples/4)];
+    samples_to_take = floor(samples_to_take);
+    
+    Blue_RSSI_filtered(k,2:samples_to_take+1) = ...
+        Blue_RSSI(k,2+samples_to_discard(1):samples_to_take+samples_to_discard(1)+1);
+    sum_index = Blue_RSSI_filtered(k,:)~=0;
+    sum_index(1) = 0;
+    Blue_RSSI_mean(k,2) = sum( Blue_RSSI_filtered(k,sum_index) ) / sum(sum_index);
+end
+end
+
