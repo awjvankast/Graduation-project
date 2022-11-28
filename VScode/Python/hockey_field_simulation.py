c0cdca7d275e609264eafa6f79ba769a2a1456bc
A.W.J. van Kasteren <bvankast@gmail.com>
2022-11-28 14:15:55 +0100

    Finalize heatmap code with correct cones

--- VScode/Python/hockey_field_simulation.py
+++ VScode/Python/hockey_field_simulation.py
@@ -6,6 +6,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 import PIL as pl
 from shapely.geometry import Polygon
+from shapely.geometry import Point
 from shapely.ops import cascaded_union
 from itertools import combinations
 from scipy import interpolate
@@ -13,17 +14,17 @@ from scipy import interpolate
 #TODO
 # Simulate with discrete steps in receiver resolution
 
-heatmap_on = 0
-animation_on = 1
+heatmap_on = 1
+animation_on = 0
 
 # Uncertainty of the direction of nodes
-res_angle = 60
+res_angle = 10
 res_angle_arr = np.arange(0, 181-res_angle, res_angle)
 
 res_angle_rad = res_angle/360*2*np.pi
 
 corner_point_coordinates = np.array([[480,780],[22,774],[28,391],[37,11],[495,16]])
-sweep_first_point_coordinates = np.array([[-100,800],[-100, -900],[8, -500],[900,-50],[800, 1000]])
+sweep_first_point_coordinates = np.array([[-1200,600],[-300, -900],[500, -5000],[900,-50],[800, 1000]])
 
 Tx_coordinates = np.array([200,200])
 
@@ -31,14 +32,18 @@ hockey_field_width = 55
 hockey_field_height = 45.8*2
 normalization_factor = hockey_field_width/458
 size_hockey_field = hockey_field_width*hockey_field_height
+
 def pixels_to_metres(pixels):
     return pixels*normalization_factor
+def metres_to_pixels(metres):
+    return metres*(1/normalization_factor)
 
 def pixels_to_metres_sqrt(pixels):
     return pixels*normalization_factor**2
 
 fig, ax = plt.subplots()
 img = plt.imread("hockey_field.png")
+
 xlim_img = img.shape[1]
 ylim_img = img.shape[0]
 plt.scatter(corner_point_coordinates[:,0],corner_point_coordinates[:,1],marker = "2",clip_on = True, s= 75)
@@ -53,14 +58,16 @@ fig.set_size_inches(15, 8)
 hockey_field_width = 55
 
 char_arr = ["B","C","E","F","G"]
+pol_tri_plot = {}
 triangle = {}
 tri_area_calc = {}
 certain_area_plot = []
 Tx_plot = []
+select_triangles = {}
 frame_number = ax.text(50,100, "", fontsize=15)
 
 def update_triangles():
-    if len(triangle) != 0:
+    if len(ax.lines) != 0:
         for k in range(0,len(char_arr)):
             ax.patches[0].remove()
         ax.lines[0].remove()
@@ -78,22 +85,25 @@ def update_triangles():
             #sec_point = np.array([np.tan(res_angle_rad)*first_point_vec[0], -pow(first_point_vec[0],2)*np.tan(res_angle_rad)/first_point_vec[1] ])
             sec_point = np.array([(first_point_vec[1]*np.tan(res_angle_rad/2)), -(first_point_vec[0]*np.tan(res_angle_rad/2))])
             #pol_tri = plt.Polygon([cur_node,cur_node+first_point_vec,sec_point+first_point_vec+cur_node],color='m', fill = True)
-            pol_tri = plt.Polygon([cur_node,cur_node+first_point_vec+sec_point, cur_node+first_point_vec-sec_point],color='m', fill = True, alpha = 0.5)
-            if j == "F":
-                ax.add_patch(pol_tri)
-            first_point = cur_node+first_point_vec-sec_point*2
+            pol_tri = Polygon([cur_node,cur_node+first_point_vec+sec_point, cur_node+first_point_vec-sec_point])
+            if pol_tri.contains(Point(Tx_coordinates[0],Tx_coordinates[1])):
+                tri_area_calc[j] = pol_tri
+                pol_tri_plot = plt.Polygon([cur_node,cur_node+first_point_vec+sec_point, cur_node+first_point_vec-sec_point],color='b', fill = True, alpha =0.3)
+                ax.add_patch(pol_tri_plot)
+            rotate_origin = np.array([np.cos(res_angle_rad)*first_point_vec[0]-np.sin(res_angle_rad)*first_point_vec[1], np.sin(res_angle_rad)*first_point_vec[0] + np.cos(res_angle_rad)*first_point_vec[1] ])
+            first_point = cur_node+rotate_origin
 
-    for j in char_arr:
-        current_node = corner_point_coordinates[char_arr.index(j)]
-        scale_factor = 100
-        Node_to_Tx_vec = (Tx_coordinates-current_node) * scale_factor
+    # for j in char_arr:
+    #     current_node = corner_point_coordinates[char_arr.index(j)]
+    #     scale_factor = 100
+    #     Node_to_Tx_vec = (Tx_coordinates-current_node) * scale_factor
 
-        point_triangle = np.array([(Node_to_Tx_vec[1]*np.tan(res_angle_rad/2)), -(Node_to_Tx_vec[0]*np.tan(res_angle_rad/2))])
-        triangle[j] = plt.Polygon( [current_node, point_triangle+Node_to_Tx_vec+current_node, -point_triangle+Node_to_Tx_vec+current_node], color='b', fill = True, alpha =0.3)
+    #     point_triangle = np.array([(Node_to_Tx_vec[1]*np.tan(res_angle_rad/2)), -(Node_to_Tx_vec[0]*np.tan(res_angle_rad/2))])
+    #     triangle[j] = plt.Polygon( [current_node, point_triangle+Node_to_Tx_vec+current_node, -point_triangle+Node_to_Tx_vec+current_node], color='b', fill = True, alpha =0.3)
         
-        tri_area_calc[j] = Polygon([current_node,point_triangle+Node_to_Tx_vec+current_node,-point_triangle+Node_to_Tx_vec+current_node])
+    #     tri_area_calc[j] = Polygon([current_node,point_triangle+Node_to_Tx_vec+current_node,-point_triangle+Node_to_Tx_vec+current_node])
 
-        #ax.add_patch(triangle[j])
+    #     #ax.add_patch(triangle[j])
 
     intersect = tri_area_calc["B"]
     def intersection(shape1, shape2):
@@ -110,7 +120,7 @@ def update_triangles():
 
     Tx_plot = ax.scatter(Tx_coordinates[0],Tx_coordinates[1] ,marker="+", s = 30, color = 'darkred')
     if heatmap_on:
-        return certain_area
+        return pixels_to_metres_sqrt(certain_area)
     else:
         return 
 
@@ -144,11 +154,11 @@ heatmap_xmax = np.max(corner_point_coordinates[:,0])-dist_node_hm
 heatmap_ymax = np.max(corner_point_coordinates[:,1])-dist_node_hm
 heatmap_data = np.empty((img.shape[1],img.shape[0]))
 heatmap_data[:] = np.nan
-pixel_res = 1
+pixel_res = 50
 
 if animation_on:
-    #ani = FuncAnimation(fig, update, frames=600, interval = 10,init_func=init, blit=True, repeat = False)
-    update_triangles()
+    ani = FuncAnimation(fig, update, frames=600, interval = 10,init_func=init, blit=True, repeat = False)
+    #update_triangles()
     #matplotlib.rcParams['animation.ffmpeg_path'] = "C:\\Users\\s153480\\Desktop\\ffmpeg-2022-11-03-git-5ccd4d3060-full_build\\bin\\ffmpeg.exe"
     #writer = animation.FFMpegWriter(fps=24, metadata=dict(artist='Me'))
     #ani.save('Hockey_field_simulation.mp4', writer = writer)
@@ -168,8 +178,33 @@ elif heatmap_on:
     ax.collections[1].remove()
 
 
-#plt.imshow(np.transpose(heatmap_data), cmap='jet', interpolation='nearest', alpha = 0.9, label = "Minimal search area [$m^2$]")
-#clrbar = plt.colorbar()
+    plt.imshow(np.transpose(heatmap_data), cmap='jet', interpolation='nearest', alpha = 0.9, label = "Minimal search area [$m^2$]")
+    clrbar = plt.colorbar()
+
+
+xax_name_list = np.array([0,10,20,30,40,50,60])
+xax_val_list = metres_to_pixels(xax_name_list)
+
+ax.xaxis.set_major_locator(matplotlib.ticker.FixedLocator((xax_val_list)))
+ax.xaxis.set_major_formatter(matplotlib.ticker.FixedFormatter((xax_name_list)))
+
+ax.set_ylim(ax.get_ylim()[::-1])
+
+yax_name_list = np.array([0,10,20,30,40,50,60,70,80,90,100,110])
+yax_val_list = metres_to_pixels(yax_name_list)
+
+ax.yaxis.set_major_locator(matplotlib.ticker.FixedLocator((yax_val_list)))
+ax.yaxis.set_major_formatter(matplotlib.ticker.FixedFormatter((yax_name_list)))
+
+
+ax.set_xlabel('X coordinates [m]')
+ax.set_ylabel('Y coordinates [m]')
+clrbar.ax.set_ylabel('Size of search area $[m^2]$')
+
+
+for item in ([ax.title, ax.xaxis.label, ax.yaxis.label, clrbar.ax.yaxis.label] + clrbar.ax.get_yticklabels() +
+             ax.get_xticklabels() + ax.get_yticklabels()):
+    item.set_fontsize(16)
 
 plt.show()
 
